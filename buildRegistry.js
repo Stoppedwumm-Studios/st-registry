f3ef5950e93e06eed2044ee76309ac41228af24a
StoppedwummPython <umber.secure@gmail.com>
2025-05-13 10:04:04 +0000

    Implement index file generation and module JSON creation; update registry configuration

--- buildRegistry.js
+++ buildRegistry.js
@@ -1,2 +1,25 @@
 const indexFile = require('./lib/indexFile');
+const path = require('path');
+const fs = require('fs');
+const modules = require("./registryConfig.json");
 
+const indexFileContent = indexFile.generateIndexFile(modules.module);
+
+fs.writeFileSync(path.join(__dirname, 'docs', 'index.json'), indexFileContent)
+
+console.log("Index file generated successfully.");
+modules.module.forEach(m => {
+    fs.mkdirSync(path.join(__dirname, 'docs', path.dirname(m.path)), { recursive: true });
+    fs.writeFileSync(path.join(__dirname, 'docs', m.path + '.json'), JSON.stringify(m, null, 2));
+    console.log(m.name + " file generated successfully.");
+});
+modules.module.forEach(m => {
+    // make query ready
+    if (typeof m.url != "string") {
+        fs.mkdirSync(path.join(__dirname, 'docs', m.path), { recursive: true });
+        m.url.forEach(u => {
+            const version = u.versionRule;
+            fs.writeFileSync(path.join(__dirname, 'docs', m.path, version + '.json'), JSON.stringify(u, null, 2));
+        })
+    }
+})
