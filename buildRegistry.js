1e91ca3d7ab7c0e0da98eaaac75dfc96e647706d
StoppedwummPython <umber.secure@gmail.com>
2025-05-13 10:50:43 +0000

    Refactor index file generation and enhance logging for better clarity

--- buildRegistry.js
+++ buildRegistry.js
@@ -1,23 +1,36 @@
+console.time("build_reg");
 const indexFile = require('./lib/indexFile');
 const path = require('path');
 const fs = require('fs');
 const modules = require("./registryConfig.json");
 
+// remove old files
+fs.rmSync(path.join(__dirname, 'docs'), { recursive: true, force: true });
+fs.mkdirSync(path.join(__dirname, 'docs'), { recursive: true });
+fs.mkdirSync(path.join(__dirname, 'docs', 'files'), { recursive: true });
+fs.mkdirSync(path.join(__dirname, 'docs', 'm'), { recursive: true });
+
+console.time("writeIndexFile");
+
 const indexFileContent = indexFile.generateIndexFile(modules.module);
 
 fs.writeFileSync(path.join(__dirname, 'docs', 'index.json'), indexFileContent)
 
+console.log("===== Index file generated successfully =====");
+console.timeEnd("writeIndexFile");
+console.log("=============================================");
+
 console.log("Index file generated successfully.");
-// remove old files
-fs.rmdirSync(path.join(__dirname, 'docs'), { recursive: true, force: true });
-fs.mkdirSync(path.join(__dirname, 'docs'), { recursive: true });
-fs.mkdirSync(path.join(__dirname, 'docs', 'files'), { recursive: true });
-fs.mkdirSync(path.join(__dirname, 'docs', 'm'), { recursive: true });
+console.time("writeModuleFiles");
 modules.module.forEach(m => {
     fs.mkdirSync(path.join(__dirname, 'docs', path.dirname(m.path)), { recursive: true });
     fs.writeFileSync(path.join(__dirname, 'docs', m.path + '.json'), JSON.stringify(m, null, 2));
     console.log(m.name + " file generated successfully.");
 });
+console.log("===== Module files generated successfully =====");
+console.timeEnd("writeModuleFiles");
+console.log("===============================================");
+console.time("writeQueryFiles");
 modules.module.forEach(m => {
     // make query ready
     if (typeof m.url != "string") {
@@ -37,8 +50,14 @@ modules.module.forEach(m => {
         fs.writeFileSync(path.join(__dirname, 'docs', m.path, "index.json"), JSON.stringify(moduleIndexFile, null, 2));
     }
 })
+console.log("===== Query files generated successfully =====");
+console.timeEnd("writeQueryFiles");
+console.log("==============================================");
 
 console.log("All files generated successfully.");
+
+// copy hosted files
+console.time("copyHostedFiles");
 
 fs.readdirSync(path.join(__dirname, "hosted")).forEach(file => {
     const filePath = path.join(__dirname, "hosted", file);
@@ -49,3 +68,10 @@ fs.readdirSync(path.join(__dirname, "hosted")).forEach(file => {
     }
 }
 );
+console.log("===== Hosted files copied successfully =====");
+console.timeEnd("copyHostedFiles");
+console.log("============================================");
+
+console.log("All files copied successfully.");
+console.log("Took:")
+console.timeEnd("build_reg");
