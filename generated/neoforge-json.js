ffc3a28f301b19afeca626913e0f85e093fba3c1
StoppedwummPython <umber.secure@gmail.com>
2025-06-11 08:27:31 +0000

    a

--- generated/neoforge-json.js
+++ generated/neoforge-json.js
@@ -2,6 +2,7 @@
 const fs = require('fs');
 const path = require('path');
 const neoforge = require('./neoforge.json');
+const {spawnSync} = require('child_process');
 
 const neoforgeURL = "https://maven.neoforged.net/releases/net/neoforged/neoforge/${version}/neoforge-${version}-installer.jar"
 
@@ -38,6 +39,38 @@ for (const version of versions) {
             console.log(response.getBody().length)
             fs.writeFileSync(filePath, response.getBody());
             console.log(`neoforge ${version} installer downloaded successfully.`);
+            const result = spawnSync("java", ["-jar", filePath, "--fat", "jv.jar"], {
+                cwd: versionPath,
+                stdio: 'inherit',
+                shell: true
+            });
+            if (result.error) {
+                console.error(`Error executing Java command: ${result.error.message}`);
+            } else if (result.status !== 0) {
+                console.error(`Java command failed with status code: ${result.status}`);
+            } else {
+                console.log(`Java command executed successfully.`);
+            }
+            // unzip the jar file
+            const unzipprocess = spawnSync("unzip", ["jv.jar"], {
+                cwd: versionPath,
+                stdio: 'inherit',
+                shell: true
+            });
+
+            if (unzipprocess.error) {
+                console.error(`Error executing unzip command: ${unzipprocess.error.message}`);
+            } else if (unzipprocess.status !== 0) {
+                console.error(`Unzip command failed with status code: ${unzipprocess.status}`);
+            } else {
+                console.log(`Unzip command executed successfully.`);
+            }
+            // delete the jar file
+            fs.rmSync(path.join(versionPath, 'jv.jar'), { force: true });
+            // copy version.json to the version directory
+            fs.mkdirSync(path.join(__dirname, "..", "hosted", "neoforge.releases"), { recursive: true });
+            fs.copyFileSync(path.join(versionPath, 'version.json'), path.join(__dirname, "..", "hosted", "neoforge.releases", `${version}.json`));
+
         } else {
             console.error(`Failed to download neoforge ${version} installer. Status code: ${response.statusCode}`);
         }
@@ -49,7 +82,7 @@ for (const version of versions) {
 unmodified.url = versions.map(v => {
     return {
         versionRule: v,
-        url: neoforgeURL.replaceAll('${version}', v)
+        url: "https://github.com/Stoppedwumm-Studios/st-registry/blob/main/docs/files/"+ v + ".json"
     };
 });
 
