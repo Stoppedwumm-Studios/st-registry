b643fef46cfb9d714ce52cb32a8150443c2f674b
StoppedwummPython <umber.secure@gmail.com>
2026-04-14 08:34:39 +0200

    Update javabase.js

--- generated/javabase.js
+++ generated/javabase.js
@@ -3,13 +3,13 @@ const path = require('path');
 
 // REMOVE the extra arguments from path.resolve
 // path.resolve only takes path segments as strings
-const targetPath = path.resolve(__dirname, "../libs/update.js");
+const targetPath = path.resolve(__dirname, "../lib/update.js");
 
 console.log("Checking path:", targetPath);
 console.log("Exists:", fs.existsSync(targetPath));
 
 // Fix the module require path (ensure it matches your file structure)
 // You had "libs/update.js" in the console, but "lib/update.js" in the require
-const updateModule = require("../libs/update.js"); 
+const updateModule = require("../lib/update.js"); 
 
 module.exports = updateModule("Stoppedwumm/JavaBase", false, "java", "jar");
