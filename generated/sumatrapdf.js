cc6621504dd128b7e5643f73ddb93766f5975d05
StoppedwummPython <umber.secure@gmail.com>
2026-04-15 12:45:51 +0200

    Add sumatrapdf.js to check file existence

---
+++ generated/sumatrapdf.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
+// DEBUG: Check if file exists relative to this script
+const targetPath = path.resolve(__dirname, "../libs/update.js");
+console.log("Checking path:", targetPath);
+console.log("Exists:", fs.existsSync(targetPath));
+
+module.exports = require("../lib/update.js")("sumatrapdfreader/sumatrapdf");
